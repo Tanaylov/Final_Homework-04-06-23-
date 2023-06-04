97830945da057957ac0dbbd43eef3a8d74a881bf
Ivan Tanajlov <wasserleu@gmail.com>
2023-06-04 16:52:44 +0300

    C#-code done

--- C#_code/Program.cs
+++ C#_code/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → [] */
+Console.Clear();
+
+string[] task = {"Написать", "программу,", "которая", "из", "имеющегося", "массива",
+                 "строк", "формирует", "новый", "массив", "из", "строк,", "длина", 
+                 "которых", "меньше,", "либо", "равна", "3", "символам."};
+
+Solution(task);
+
+void Solution(string[] array)
+{
+  byte size = (byte) new Random().Next(0, 4);
+  string[] result = new string[size];
+  for(byte i = 0; i < size; i++)
+    {
+      byte index = (byte) new Random().Next(0, array.Length);
+      result[i] = array[index];
+    } 
+  Console.WriteLine("[" + String.Join(" ", array) + "]" + " -> " + "[" + String.Join(" ", result) + "]");
+}
